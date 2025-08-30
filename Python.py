2e79229e99a764692f3ce425d9398d871cae6b83
pavithk7772 <pavithkumarkv@gmail.com>
2025-08-30 15:13:42 +0530

    my first code

---
+++ Python.py
@@ -0,0 +1,10 @@
+# Sum of two numbers in Python
+
+def sum_of_two_numbers(a, b):
+    return a + b
+
+# Example usage
+num1 = 5
+num2 = 7
+result = sum_of_two_numbers(num1, num2)
+print("The sum is:", result)
